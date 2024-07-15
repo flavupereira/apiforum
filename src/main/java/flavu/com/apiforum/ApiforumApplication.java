84a9a5b9015a6f0471cad15c3b2ff20132c1299b
flavupereira <flavupereira@gmail.com>
2024-07-14 22:30:21 -0300

    first commit

---
+++ src/main/java/flavu/com/apiforum/ApiforumApplication.java
@@ -0,0 +1,13 @@
+package flavu.com.apiforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiforumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiforumApplication.class, args);
+	}
+
+}
